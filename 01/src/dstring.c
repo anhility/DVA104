1c7dfc4f953a511635bc9b31e323df2c9085dded
anhility <anhility@gmail.com>
2019-04-04 21:18:03 +0200

    New workday, new resources

--- 01/src/dstring.c
+++ 01/src/dstring.c
@@ -8,18 +8,18 @@ DString dstring_initialize(const char* str)
 {
 	// Precondition: str är ej NULL
   assert(str != NULL);
+
 	/* Tips:
 	   - Allokera tillräckligt med minne för att få plats med str
 	   - Det är tillåtet att använda inbyggda strängfunktioner (som t.ex strcpy())
 	   - Kom ihåg att använda assert() */
-  char *p = malloc(strlen(str) + 1);
+  DString p = (DString)malloc(strlen(str));
   assert(p != NULL);
 
   strcpy(p, str);
-  printf("%s",p);
 
 	// Postcondition: returvärdet innehåller samma sträng som 'str'
-  assert(p == str);
+  assert(strcmp(p, str) == 0);
 
   return p;
 }
@@ -29,15 +29,25 @@ int dstring_concatenate(DString* destination, DString source)
 	// Precondition: destination är ej NULL
 	// Precondition: *destination är ej NULL
 	// Precondition: source är ej NULL
+  assert(destination != NULL);
+  assert(*destination != NULL);
+  assert(source != NULL);
 
 	/* Tips: 
 	   - Tänk på att *destination redan är en dynamiskt allokerad sträng
 	   - Tänk på att vi kommer att behöva allokera mer minne för att få plats med den här längre strängen. 
 	   */
+  int s_size = strlen(*destination) + strlen(source);
+  destination = (DString*)realloc(destination, s_size);
+  assert(destination != NULL);
+
+  DString tmp = *destination;
+  *destination = strcat(*destination, source);
 
 	// Post condition: *destination innehåller den gamla strängen ihopslagen med source
+  assert(strcmp(*destination, strcat(tmp, source)) == 0);
 
-	return -1; // Ersätt denna rad. Den är just nu med för att programmet ska kompilera.
+  return 1;
 }
 
 void dstring_truncate(DString* destination, unsigned int truncatedLength)
